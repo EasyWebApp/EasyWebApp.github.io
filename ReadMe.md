154ca17abb9fe9570bc56d9303cf89164b1f5797
TechQuery <shiy007@qq.com>
2019-05-19 04:15:54 +0800

    [ Add ]  2 Context sections [ Optimize ]  Upgrade Upstream libraries

--- ReadMe.md
+++ ReadMe.md
@@ -1,3 +1,6 @@
 # WebCell official website
 
-Design of https://web-cell.tk/ is based on [Product](https://getbootstrap.com/docs/4.1/examples/product/) & [Carousel](https://getbootstrap.com/docs/4.1/examples/carousel/) templates of BootStrap 4.
+Design of https://web-cell.dev/ is based on [Product][1] & [Carousel][2] templates of BootStrap 4.
+
+[1]: https://getbootstrap.com/docs/4.1/examples/product/
+[2]: https://getbootstrap.com/docs/4.1/examples/carousel/
--- page/index.js
+++ page/index.js
@@ -1,22 +1,52 @@
-(function($, WebCell, Loading) {
+(function($, WebCell, DOM_Renderer, Loading) {
     var request = WebCell.request,
         documentReady = WebCell.documentReady,
         mediaReady = WebCell.mediaReady,
-        ObjectView = WebCell.ObjectView;
+        View = DOM_Renderer.default;
 
-    Promise.all([request("page/index.json"), documentReady])
+    function render(element, data) {
+        var template = View.getTemplate(element);
+
+        data = data instanceof Array ? data : [data];
+
+        return Promise.all(
+            data.map(function(item) {
+                var view = new View(template);
+
+                return view.render(item).then(function() {
+                    return view.topNodes;
+                });
+            })
+        ).then(function(list) {
+            element.append.apply(element, [].concat.apply([], list));
+        });
+    }
+
+    Promise.all([request('page/index.json'), documentReady])
         .then(function(data) {
-            var view = new ObjectView($("body > footer")[0]);
+            data = data[0].body;
 
-            view.render(data[0]);
-
-            return mediaReady();
+            return Array.prototype.reduce.call(
+                document.querySelectorAll('[data-key]'),
+                function(promise, element) {
+                    return promise.then(
+                        render.bind(null, element, data[element.dataset.key])
+                    );
+                },
+                Promise.resolve()
+            );
         })
-        .then(Loading.closeAll.bind(Loading));
+        .then(mediaReady);
+    //  .then(Loading.closeAll.bind(Loading));
 
-    $(document).on("click", 'footer a[href="#top"]', function() {
+    $(document).on('click', 'footer a[href="#top"]', function() {
         self.scrollTo(0, 0);
 
         return false;
     });
-})(self.jQuery, self["web-cell"], self["Loading"].default);
+})(
+    self.jQuery,
+    self['web-cell'],
+    self['dom-renderer']
+    // self['Loading'].default
+);
